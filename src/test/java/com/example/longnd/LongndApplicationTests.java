31e60440078f0c247ff0bc2ecbd92c0450d0f921
Dinh Long <itlong94@gmail.com>
2020-07-11 13:40:32 +0900

    Update cmd

--- src/test/java/com/example/longnd/LongndApplicationTests.java
+++ src/test/java/com/example/longnd/LongndApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.longnd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class LongndApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test() {
+        assertThat("test".getClass().getName()).isEqualTo("java.lang.String");
     }
 
 }
