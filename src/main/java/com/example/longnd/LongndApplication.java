be9972e64e80ce15efb2616beb2b56b5304f1eed
Dinh Long <itlong94@gmail.com>
2020-07-11 12:07:38 +0900

    init

---
+++ src/main/java/com/example/longnd/LongndApplication.java
@@ -0,0 +1,13 @@
+package com.example.longnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/longnd/LongndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.longnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
